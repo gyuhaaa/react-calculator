37103cb256c60f2eaf51f39616941b4063c0ae6d
gyuhaaa <vkvkvk2892@gmail.com>
2024-05-10 14:49:57 +0900

    onClickButton - Number

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,20 @@
-const Button = ({ name }) => {
-    return (
-      <button className="bg-gray-200 w-10 h-10 rounded-md flex justify-center items-center shadow-sm">
-        {name}
-      </button>
-    );
+const Button = ({ name, result, setResult }) => {
+  const onClickButton = () => {
+    if (result === "0") {
+      setResult(name);
+    } else {
+      setResult(result + name);
+    }
   };
-  
-  export default Button;
+
+  return (
+    <button
+      className="bg-gray-200 w-10 h-10 rounded-md flex justify-center items-center shadow-sm"
+      onClick={onClickButton}
+    >
+      {name}
+    </button>
+  );
+};
+
+export default Button;
